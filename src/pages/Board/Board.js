31c4ac8ef810f6c8fe59c8ebd27253b99d26ec46
nur <mirov0727@gmail.com>
2023-05-05 14:50:45 +0800

    making the board dynamic (drag and drop task)

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import useDataFetching from '../../hooks/useDataFetching';
 import Lane from '../../components/Lane/Lane';
 import { Link } from 'react-router-dom';
@@ -10,9 +11,37 @@ const lanes = [
   { id: 4, title: 'Done' },
 ];
 
+function onDragStart(e, id) {
+  e.dataTransfer.setData('id', id);
+}
+
+function onDragOver(e) {
+  e.preventDefault();
+};
+
 function Board() {
-  const [loading, error, tasks] =
+  const [loading, error, data] =
     useDataFetching(`https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`);
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, [data]);
+
+  function onDrop(e, laneId) {
+    const id = e.dataTransfer.getData('id');
+  
+    const updateTasks = tasks.filter((task) => {
+      if (task.id.toString() === id) {
+        task.lane = laneId;
+      }
+
+      return task;
+    });
+
+    setTasks(updateTasks);
+  }
+
   return (
     <div>
       <button>
@@ -22,10 +51,14 @@ function Board() {
         {lanes.map((lane) => (
           <Lane
             key={lane.id}
+            laneId={lane.id}
             title={lane.title} 
             loading={loading}
             error={error}
             tasks={tasks.filter((task) => task.lane === lane.id)}
+            onDragStart={onDragStart}
+            onDragOver={onDragOver}
+            onDrop={onDrop}
           />
         ))}
       </div>
